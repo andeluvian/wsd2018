9b4f66d824e38327bf4c0003b8a6e4528e3f20da
andeluvian <kenneth.forsman@gmail.com>
2018-11-18 16:36:07 +0200

    geometry app

---
+++ exercises/03_python/geometry.py
@@ -0,0 +1,21 @@
+import math
+
+class Point:
+	def __init__(self, p1,p2):
+		self.p1 = p1
+		self.p2 = p2
+		
+	def distance_form(self,point)
+		dist = math.sqrt((point.p1 - self.p1)**2 + (point.p2 - self.p2)**2)
+		return dist
+		
+class Circle:
+	def __init__(self,center ,radius):
+		self.center = center
+		self.radius = radius
+	
+	def is_inside(self,point)
+		if ((point.p1 - self.center.p1) * (point.p1 - self.center.p1) + (point.p2 - self.center.p2) * (point.p2 - self.center.p2) <= self.radius * self.radius):
+			return True
+		else:
+			return False
