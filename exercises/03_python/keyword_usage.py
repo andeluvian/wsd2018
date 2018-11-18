7123e40befc8e3bc43e3fdb11ebb9c1d04d9eed4
andeluvian <kenneth.forsman@gmail.com>
2018-11-18 15:20:37 +0200

    initial test

--- exercises/03_python/keyword_usage.py
+++ exercises/03_python/keyword_usage.py
@@ -1,8 +1,17 @@
+
 def keyword_usage(sentence, list_of_words):
   my_tuple = ()
-  for word in sentence.split(' '):
+  for word in sentence.split(" "):
     if word in list_of_words:
-      my_tuple = my_tuple + (true,)
+      my_tuple = my_tuple + (True,)
     else:
-      my_tuple = my_tuple + (false,)
+      my_tuple = my_tuple + (False,)
   return my_tuple
+
+  
+def main():
+	sentence = input("")
+	list_of_words = ['Python',  'python', 'scala']
+	print(keyword_usage(sentence, list_of_words))
+	
+main()
