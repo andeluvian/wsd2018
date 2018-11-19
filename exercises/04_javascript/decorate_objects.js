cb2165a21f44153e65f4fd682dee9ba512a4093a
andeluvian <kenneth.forsman@gmail.com>
2018-11-19 20:58:23 +0200

    corrected person class

--- exercises/04_javascript/decorate_objects.js
+++ exercises/04_javascript/decorate_objects.js
@@ -1,44 +1,39 @@
-var addPersonMethods = function (person){
-
-var person = {};
-person.name = name;
-person.age = age;
-
-console.log('Hi! I\'m ' + this.name + '.');
+function addPersonMethods(person){
 
 
+this.name = person.name;
+this.age = person.age;
 
-this.greet = function(sentence)
-{
-return sentence + ", my name is " + this.name
-}
-
-this.compareage = function(person){
-
-if (this.age < person.age)
-{
-return "My name is " + this.name + " and I'm younger than " +person.name
-}
-if(this.age == person.age)
-{
-return "My name is " +this.name + " and I'm as young as " +person.name
-}
-else
-{
-return "My name is " +this.name + " and I'm older than " +person.name
-}
-}
-
-this.namesake = function (person){
-
-if(this.name == person.name){
-
-  return "We have the same name, " + person.name + " and I!"
-}
-else
-{
-  return "We have different names, " + person.name + " and I."
-}
-
-}
+person.greet = function (sentence){
+return sentence + ", my name is " + person.name;
 };
+
+person.compareage = function(person){
+  if (this.age < person.age)
+  {
+  return "My name is " + this.name + " and I'm younger than " +person.name
+  }
+  if(this.age == person.age)
+  {
+  return ["My name is " +this.name + " and I'm as young as " +person.name].join("");
+  }
+  else
+  {
+  return ["My name is " +this.name + " and I'm older than " +person.name].join("");
+  }
+};
+
+person.namesake = function(person){
+  if(this.name == person.name)
+  {
+  return ["We have the same name, " + person.name + " and I!"].join("");
+  }
+  else
+  {
+  return ["We have different names, " + person.name + " and I."].join("");
+  }
+};
+
+
+return person;
+}
