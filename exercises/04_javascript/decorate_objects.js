04f618ac3876a129a66d87fb249416697b8dbdc5
andeluvian <kenneth.forsman@gmail.com>
2018-11-19 21:24:58 +0200

    corrected person class

--- exercises/04_javascript/decorate_objects.js
+++ exercises/04_javascript/decorate_objects.js
@@ -13,25 +13,25 @@ person.compareAge = function(person){
   }
   if(this.age == person.age)
   {
-  return ["My name is " +this.name + " and I'm as young as " +person.name].join("");
+  return "My name is " +this.name + " and I'm as young as " +person.name
   }
   else
   {
-  return ["My name is " +this.name + " and I'm older than " +person.name].join("");
+  return "My name is " +this.name + " and I'm older than " +person.name
   }
 };
 
 person.namesake = function(person){
   if(this.name == person.name)
   {
-  return ["We have the same name, " + person.name + " and I!"].join("");
+  return ["We have the same name, " + person.name + " and I!"
   }
   else
   {
-  return ["We have different names, " + person.name + " and I."].join("");
+  return "We have different names, " + person.name + " and I."
   }
 };
 
 
-return person;
+console.log(person);
 }
