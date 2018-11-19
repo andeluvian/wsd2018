bfde44c13d326778d78b4485080e89a83d741846
andeluvian <kenneth.forsman@gmail.com>
2018-11-19 21:43:37 +0200

    corrected person class

--- exercises/04_javascript/decorate_objects.js
+++ exercises/04_javascript/decorate_objects.js
@@ -3,35 +3,36 @@ this.name = person.name;
 this.age = person.age;
 
 person.greet = function (sentence){
-return sentence + ", my name is " + person.name;
+console.log(sentence + ", my name is " + person.name);
 };
 
 person.compareAge = function(person){
   if (this.age < person.age)
   {
-  return "My name is " + this.name + " and I'm younger than " +person.name
+  console.log(["My name is " + this.name + " and I'm younger than " +person.name].join(""));
   }
   if(this.age == person.age)
   {
-  return "My name is " +this.name + " and I'm as young as " +person.name
+  console.log(["My name is " +this.name + " and I'm as young as " +person.name].join(""));;
   }
-  else
+  if(this.age >= person.age)
   {
-  return "My name is " +this.name + " and I'm older than " +person.name
+  console.log(["My name is " +this.name + " and I'm older than " +person.name].join(""));;
   }
 };
 
 person.namesake = function(person){
   if(this.name == person.name)
   {
-  return ["We have the same name, " + person.name + " and I!"
+  console.log(["We have the same name, " + person.name + " and I!"].join(""));
   }
   else
   {
-  return "We have different names, " + person.name + " and I."
+  console.log(["We have different names, " + person.name + " and I."].join(""));
   }
 };
 
 
-console.log(person);
-};
+
+return person;
+}
