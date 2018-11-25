ac103bffe386d0f8344e6523cec02136a783f452
andeluvian <kenneth.forsman@gmail.com>
2018-11-25 11:05:08 +0200

    django app

--- exercises/04_javascript/exercises.js
+++ exercises/04_javascript/exercises.js
@@ -1,44 +1,123 @@
 function keywordusage (text, keywords){
+  var results =[];
+  var split_text = text.split(" ");
 
-return "hello"
+  for(word in keywords){
+
+    if(split_text.indexOf(word) > -1)
+    {
+      results.push("True")
+    }
+    else
+    {
+      results.push("False")
+    }
+
+  }
+  return results
 }
 
 
 function frequencies(text,wordlist){
   var results = {};
-  var a = text.split(" ");
-  var n = 0;
-for ( i < wordlist.length){
-if(wordlist[i].count){
+  var sentence = text.split(" ");
 
-results.push(a[i],n);
+  for(keyword in wordlist){
+    results[keyword] = 0;
+
+    var filter_sentence = sentence.filter(function(word)
+    {
+
+      if(keyword == word){
+      return true;
+      }
+      else
+      {
+
+      return false;
+      }
+
+
+    }
+
+  )
+
+
 }
-i++
-}
+
+
 return results
 
 }
 
 
-function rotate(array,steps){
-  if(array.length < 2) {
-      return array.slice(0);
+function rotate(array, steps) {
+    var removeArray = [];
+
+
+
+    if (steps < 0)
+        {var y = 0
+
+        while (y > steps)
+            {
+            removeArray = array.splice(0, 1);
+            var pos = array.length - 1;
+            array.splice(pos, 0, removeArray[0]);
+
+
+        y--;
+        }
     }
 
-    var n = steps % array.length;
+    else
+    {
+      var x = 0
+    while ( x < steps)
+        {
+        removeArray = array.splice(3, 1);
+        array.splice(0, 0, removeArray[0]);
+        x++;
 
-    if(n === 0) {
-      return array.slice(0);
+        }
+
+
     }
 
 
-    if(n < 0) {
-      return array.slice(n).concat(array.slice(0, array.length+n));
-    } else {
-      return array.slice(n).concat(array.slice(0, n));
-    }
+    return array;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+  /*if(array.length < 2) {
+    return array.slice(0);
+
+  }
+
+  var n = steps % array.length;
+
+  if(n === 0) {
+    return array.slice(0);
   }
 
 
+  if(n < 0) {
+    return array.slice(n).concat(array.slice(0, array.length+n));
+  } else {
+    return array.slice(n).concat(array.slice(0, n));
+  }
+}
+
+*/
 
 }
--- exercises/05_django/firstdjango/firstdjango/urls.py
+++ exercises/05_django/firstdjango/firstdjango/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from webshop import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+	path('about/',views.about,name='about'),
+	path('products/',views.available_products,name='products'),
+	path('products/01',views.productview,name='productview')
 ]
